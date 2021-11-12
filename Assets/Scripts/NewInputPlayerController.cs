588943b27f59c70d652024fff2d866d47c51a398
Mac242 <72981027+Mac242@users.noreply.github.com>
2021-11-12 18:36:46 +0100

    new playerInputController

---
+++ Assets/Scripts/NewInputPlayerController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class NewInputPlayerController : MonoBehaviour
+{
+    private Vector2 movementInput;
+    public float speed = 200f;
+    private float horizontalInput; 
+    private float verticalInput;    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.Translate(Vector2.right * Time.deltaTime * speed * movementInput.x);
+        transform.Translate(Vector2.up * Time.deltaTime * speed * movementInput.y);
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        movementInput = context.ReadValue<Vector2>();
+    }
+}
