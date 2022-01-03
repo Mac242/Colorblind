4f425e9e770967fd52ce90ce0bc67c4c0f3bedf0
Anna Moritz <72981027+Mac242@users.noreply.github.com>
2022-01-03 16:12:31 +0100

    add scene change for jump n run

---
+++ Assets/Scripts/SceneChange_JumpnRun.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange_JumpnRun : MonoBehaviour
+{
+    [SerializeField] GameObject playerRed;
+	[SerializeField] GameObject playerGreen;
+	private bool greenIn;
+	private bool redIn;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+	    greenIn = false;
+	    redIn = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+	    if (other.gameObject == playerGreen)
+	    {
+		    Debug.Log("GreenIn");
+		    greenIn = true;
+	    }
+	    
+	    if (other.gameObject == playerRed)
+	    {
+		    Debug.Log("redIn");
+		    redIn = true;
+	    }
+
+	    if (greenIn == true && redIn == true)
+	    {
+		    SceneManager.LoadScene("Scenes/Scene_Begin to Play");
+	    }
+    }
+}
