5709563fabbab215499ab832bb6df1021f50429b
Anna Moritz <72981027+Mac242@users.noreply.github.com>
2022-01-19 11:31:16 +0100

    add new scenes with diffrent difficulties

--- Assets/Scripts/PUZZLE_C.cs
+++ Assets/Scripts/PUZZLE_C.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 
 public class PUZZLE_C : MonoBehaviour
 {
+    public Transform parent;
+    public PolygonCollider2D top;
+    public PolygonCollider2D bottom;
+    public PolygonCollider2D complete;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        top.enabled = true;
+        bottom.enabled = true;
+        complete.enabled = false;
     }
 
     // Update is called once per frame
@@ -21,6 +28,10 @@ public class PUZZLE_C : MonoBehaviour
         if (other.gameObject.CompareTag("C"))
         {
             Debug.Log("C Created");
+            transform.SetParent(parent);
+            top.enabled = false;
+            bottom.enabled = false;
+            complete.enabled = true;
         }
     }
 }
--- Assets/Scripts/Respawn_Labyrint.cs
+++ Assets/Scripts/Respawn_Labyrint.cs
@@ -10,6 +10,8 @@ public class Respawn_Labyrint : MonoBehaviour
     public GameObject player2;
     public Transform spawnPoint1;
     public Transform spawnPoint2;
+    [SerializeField] private GameObject lives2;
+    [SerializeField] private GameObject lives1;
     [SerializeField] int numberOfAllowedCollisions;
     
     void Start()
@@ -24,17 +26,40 @@ public class Respawn_Labyrint : MonoBehaviour
             player1.transform.position = spawnPoint1.transform.position;
             player2.transform.position = spawnPoint2.transform.position;
             numberOfAllowedCollisions = 3;
-
         }
+
+        if (numberOfAllowedCollisions == 3)
+        {
+            lives2.SetActive(false);
+            lives1.SetActive(false);
+        }
+        
+        if (numberOfAllowedCollisions == 2)
+        {
+            lives2.SetActive(true);
+        }
+
+        if (numberOfAllowedCollisions == 1)
+        {
+            lives1.SetActive(true);
+        }
+        
     }
 
     private void OnCollisionEnter2D(Collision2D col)
     {
-        if (col.gameObject.CompareTag("Respawn"))
+        if (col.gameObject.CompareTag("Respawn") && gameObject == player1)
         {
-            Gamepad.current.SetMotorSpeeds(.5f, 1f);
+            Gamepad.current.SetMotorSpeeds(.5f, .8f);
             numberOfAllowedCollisions -= 1;
-            Debug.Log("Collision");
+            Debug.Log("Collision Player 1");
+        }
+        
+        if (col.gameObject.CompareTag("Respawn") && gameObject == player2)
+        {
+            Gamepad.current.SetMotorSpeeds(.5f, .8f);
+            numberOfAllowedCollisions -= 1;
+            Debug.Log("Collision Player 2");
         }
         
         
@@ -42,7 +67,12 @@ public class Respawn_Labyrint : MonoBehaviour
     
     private void OnCollisionExit2D(Collision2D col)
     {
-        if (col.gameObject.CompareTag("Respawn"))
+        if (col.gameObject.CompareTag("Respawn") && gameObject== player1)
+        {
+            Gamepad.current.SetMotorSpeeds(0f, 0f);
+        }
+        
+        if (col.gameObject.CompareTag("Respawn") && gameObject== player2)
         {
             Gamepad.current.SetMotorSpeeds(0f, 0f);
         }
